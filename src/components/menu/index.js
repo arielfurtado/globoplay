78edc0205f143870601458ee3e7348fcac25fafc
lucio.furtado <lucio.furtado@wooza.com.br>
2020-05-21 17:58:40 -0300

    feat: cria menu da aplicação

---
+++ src/components/menu/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import * as S from './styles';
+import { MdHome, MdSearch, MdSurroundSound, MdVideoLibrary, MdAccountCircle } from "react-icons/md";
+
+const Menu = ({keyPressed, hasFocus, closeMenu, lastFocus}) => {
+    const [menuItemFocus, setMenuItemFocus] = useState(1);
+    const [navItems] = useState([
+        {
+          id: 1,
+          icon: <MdSearch/>,
+          name: "Busca"
+        },
+        {
+          id: 2,
+          icon: <MdHome/>,
+          name: "Início"
+        },
+        {
+          id: 3,
+          icon: <MdSurroundSound/>,
+          name: "Agora na Globo"
+        },
+        {
+          id: 4,
+          icon: <MdVideoLibrary/>,
+          name: "Categorias"
+        },
+        {
+          id: 5,
+          icon: <MdAccountCircle/>,
+          name: "Minha conta"
+        },
+    ]);
+
+
+    useEffect(() => {
+      if(hasFocus) {
+        if(keyPressed === 'ArrowDown' && menuItemFocus < navItems.length) {
+          setMenuItemFocus(menuItemFocus + 1);
+        }
+        if(keyPressed === 'ArrowUp' && menuItemFocus > 1 ) {
+          setMenuItemFocus(menuItemFocus - 1);
+        }
+        if(keyPressed === 'ArrowRight') {
+          return closeMenu(lastFocus);
+        }
+      }
+      }, [keyPressed, menuItemFocus, navItems, closeMenu, hasFocus, lastFocus]);
+
+    return(
+        <S.Nav focused={hasFocus} >
+            <S.List id="navigation">
+                {navItems.map((item, index) => {
+                    return (
+                        <S.ListItem key={index} id={item.id} isActive={menuItemFocus === item.id && hasFocus}>
+                            {item.icon}
+                            <span>{item.name}</span>
+                        </S.ListItem>
+                    )
+                })}
+
+            </S.List>
+        </S.Nav>
+    )
+};
+
+export default Menu
---
+++ src/components/menu/styles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav `
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+    background: #060406;
+    position: absolute;
+    max-width: min-content;
+    z-index: 9;
+    ${props => props.focused &&`
+        li {
+            span {
+                max-width: 150px;
+            }
+        }
+    `}
+`;
+export const List = styled.ul `
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+export const ListItem = styled.li `
+    color: white;
+    font-size: 20px;
+    white-space: nowrap;
+    display: flex;
+    align-items: center;
+    padding: 30px;
+    overflow: hidden;
+    min-width: 90px;
+    span {
+        margin-left: 10px;
+        white-space: nowrap;
+        transition: all 0.3s;
+        max-width: 0;
+        overflow: hidden;
+    }
+    ${props => props.isActive &&`
+            background: white;
+            color: black;
+        `
+    }
+`;
